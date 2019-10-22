0e1db78cf2057783abb87edf67d87d3ab6d6fd61
Joshua Senouf <joshua.senouf@gmail.com>
2019-10-22 17:23:32 -0400

    Adding MIT license. Misc changes in the Bash scripts

--- configure.sh
+++ configure.sh
@@ -41,8 +41,8 @@ cd ${BUILD_PATH}
 cmake \
     ${BUILD_PATH}/.. \
     -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} \
-    -DCMAKE_C_FLAGS=-fPIC \
-    -DCMAKE_CXX_FLAGS=-fPIC
+    -DCMAKE_C_FLAGS="-fPIC" \
+    -DCMAKE_CXX_FLAGS="-fPIC"
 
 echo -e "\n"
 echo -e "[CONFIGURE] Finished configuring Ptex-${PTEX_VERSION}!"
